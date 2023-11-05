6b8b1598fa073cfc78cb7206aa0005e9b70b4550
Soham Ghugare <soham.ghugare@gmail.com>
2023-11-05 22:43:05 +0530

    feat: added ground

---
+++ src/Ground.js
@@ -0,0 +1,30 @@
+import { MeshReflectorMaterial } from "@react-three/drei";
+
+export function Ground() {
+    return (
+        <mesh rotateX={-Math.PI * 0.5} castShadow receiveShadow>
+            <planeGeometry args={[30, 30]} />
+            <MeshReflectorMaterial
+                envMapIntensity={0}
+                normalMap={normal}
+                normalScale={[0.15, 0.15]}
+                roughnessMap={roughness}
+                dithering={true}
+                color={[0.015, 0.015, 0.015]}
+                roughness={0.7}
+                blur={[1000, 400]} // Blur ground reflections (width, heigt), 0 skips blur
+                mixBlur={30} // How much blur mixes with surface roughness (default = 1)
+                mixStrength={80} // Strength of the reflections
+                mixContrast={1} // Contrast of the reflections
+                resolution={1024} // Off-buffer resolution, lower=faster, higher=better quality, slower
+                mirror={0} // Mirror environment, 0 = texture colors, 1 = pick up env colors
+                depthScale={0.01} // Scale the depth factor (0 = no depth, default = 0)
+                minDepthThreshold={0.9} // Lower edge for the depthTexture interpolation (default = 0)
+                maxDepthThreshold={1} // Upper edge for the depthTexture interpolation (default = 0)
+                depthToBlurRatioBias={0.25} // Adds a bias factor to the depthTexture before calculating the blur amount 
+                debug={0}
+                reflectorOffset={0.2} // Offsets the virtual camera that projects the reflection. Useful when the reflective surface is some distance from the object's origin (default = 0)
+            />
+        </mesh>
+    )
+}
