3f1de7c5bca98f78a594adb649773eea2f6837df
Soham Ghugare <soham.ghugare@gmail.com>
2023-11-05 23:30:19 +0530

    feat: added ganesh model

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import "./style.css";
 import {
   EffectComposer,
-  DepthOfField,
-  Bloom,
   ChromaticAberration,
 } from "@react-three/postprocessing";
 import { BlendFunction } from "postprocessing";
-import { CubeCamera, Environment, OrbitControls, PerspectiveCamera } from '@react-three/drei';
-import { Ground } from './Ground';
-import { Car } from './Car';
+import {
+  CubeCamera,
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import "./style.css";
+import { Car } from "./Car";
+import { Ground } from "./Ground";
 
-function ModelViewer(){
+function CarShow() {
   return (
     <>
-      {/* orbit controls */}
-      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+      <OrbitControls 
+        target={[0, 0.35, 0]}
+        maxPolarAngle={1.45}
+      />
 
-      {/* camera */}
-      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+      <PerspectiveCamera makeDefault fov={40} position={[5, 5, -15]} />
 
-      {/* background color */}
       <color args={[0, 0, 0]} attach="background" />
 
-      {/* Cube Camera */}
-      <CubeCamera resolution={256} frames={Infinity}>
+      <CubeCamera resolution={1024} frames={Infinity}>
         {(texture) => (
           <>
             <Environment map={texture} />
@@ -34,57 +36,27 @@ function ModelViewer(){
         )}
       </CubeCamera>
 
-      {/* spotlights */}
-      <spotLight
-        color={[1, 0.25, 0.7]}
-        intensity={1.5}
-        angle={0.6}
-        penumbra={0.5}
-        position={[5, 5, 0]}
-        castShadow
-        shadow-bias={-0.0001}
-      />
-      <spotLight
-        color={[0.14, 0.5, 1]}
-        intensity={2}
-        angle={0.6}
-        penumbra={0.5}
-        position={[-5, 5, 0]}
-        castShadow
-        shadow-bias={-0.0001}
-      />
+      <ambientLight />
       <Ground />
 
       <EffectComposer>
-        {/* <DepthOfField focusDistance={0.0035} focalLength={0.01} bokehScale={3} height={480} /> */}
-        <Bloom
-          blendFunction={BlendFunction.ADD}
-          intensity={1.3} // The bloom intensity.
-          width={300} // render width
-          height={300} // render height
-          kernelSize={5} // blur kernel size
-          luminanceThreshold={0.15} // luminance threshold. Raise this value to mask out darker elements in the scene.
-          luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
-        />
         <ChromaticAberration
           blendFunction={BlendFunction.NORMAL} // blend mode
           offset={[0.0005, 0.0012]} // color offset
         />
       </EffectComposer>
     </>
-  )
+  );
 }
 
 function App() {
   return (
     <Suspense fallback={null}>
       <Canvas shadows>
-        <ModelViewer />
+        <CarShow />
       </Canvas>
     </Suspense>
-  )
+  );
 }
 
 export default App;
-
-
--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ import App from './App';
 import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />)
-
-
+root.render(<App />);
